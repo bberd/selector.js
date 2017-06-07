e0afb6e9dd9cc8bee86ca44bb92ace1fa65228c0
Boris Berdichevskiy <bberdich@gmail.com>
2017-06-07 18:26:34 -0400

    almost there, but not really

--- src/selector.js
+++ src/selector.js
@@ -3,12 +3,33 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
   if (typeof startEl === "undefined") {
     startEl = document.body;
+
   }
 
   // traverse the DOM tree and collect matching elements in resultSet
   // use matchFunc to identify matching elements
 
   // YOUR CODE HERE
+
+  //console.log(startEl.children);
+    function GoDownTree(startEl) {
+
+    let childArr = [].slice.call(startEl.children);
+
+
+    if (childArr.length > 0) {
+      traverseDomAndCollectElements(matchFunc, childArr.slice(1));
+      if (matchFunc(childArr[0])) {
+        resultSet.push(childArr[0]);
+        console.log(resultSet);
+      }
+    } else {
+      resultSet.push(startEl);
+    }
+  }
+
+  GoDownTree(startEl);
+
 
   return resultSet;
 };
@@ -18,6 +39,26 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 // return one of these types: id, class, tag.class, tag
 
 var selectorTypeMatcher = function(selector) {
+  var tag = false;
+  var hashtag = false;
+  for (var i = 0; i < selector.length; i++) {
+    if (selector[i] === ".") {
+      tag = true;
+    }
+    if (selector[i] === "#") {
+      hashtag = true;
+    }
+  }
+
+  if (selector[0] === "#") {
+    return "id";
+  } else if (selector[0] === ".") {
+    return "class";
+  } else if (tag && selector[0] != ".") {
+    return "tag.class";
+  } else if (!hashtag && !tag) {
+    return "tag";
+  }
   // your code here
 };
 
@@ -32,17 +73,43 @@ var matchFunctionMaker = function(selector) {
   var matchFunction;
   if (selectorType === "id") {
     // define matchFunction for id
-
+    matchFunction = function(element) {
+      return element.id === selector.slice(1);
+    }
   } else if (selectorType === "class") {
     // define matchFunction for class
-
+    matchFunction = function(element) {
+      for(let i = 0; i < element.className.split(' ').length; i++) {
+        if(element.className.split(' ')[i] === selector.slice(1))
+          return true;
+      }
+      return false;
+    }
   } else if (selectorType === "tag.class") {
-    // define matchFunction for tag.class
+    //define matchFunction for tag.class
+      matchFunction = function(element) {
+
+        let selector1 = selector.split('.');
+        let tag = selector1[0]; // elector img
+        let classN = selector1[1]; // selector thumbail
+
+        var found = false;
+
+        for(let i = 0; i < element.className.split(' ').length; i++) {
+          if(element.className.split(' ')[i] === classN)
+            found = true;
+        }
+        return (element.tagName && element.tagName.toLowerCase() === tag.toLowerCase() && found === true);
+
+       }
+
 
   } else if (selectorType === "tag") {
-    // define matchFunction for tag
-
+    matchFunction = function(element) {
+      return element.tagName && element.tagName.toLowerCase() === selector.toLowerCase();
+    }
   }
+
   return matchFunction;
 };
 
